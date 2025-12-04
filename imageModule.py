14b9afd76dc4667345b43df745f5f702aab7aa54
haikalfiqri <haikalfiqri.ramdhan@gmail.com>
2025-12-04 19:31:58 +0900

    the core code uploaded

---
+++ imageModule.py
@@ -0,0 +1,15 @@
+# imagepreproc_haikalfiqri/preprocessing.py
+
+import cv2
+import numpy as np
+from PIL import Image
+
+
+# ---------- Color conversion ----------
+#convert color conversion of an image
+
+# ---------- Noise reduction ----------
+#convert a noisy image to a denoised image
+
+# ---------- Resizing ----------
+#convert an image to a resized image
