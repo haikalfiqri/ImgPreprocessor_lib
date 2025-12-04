af1426b831db99bc64d242a233c4005d95706730
Akif Najwan <akifnajwan43@gmail.com>
2025-12-05 01:03:03 +0900

    update on image denoise

--- imageModule.py
+++ imageModule.py
@@ -11,5 +11,14 @@ from PIL import Image
 # ---------- Noise reduction ----------
 #convert a noisy image to a denoised image
 
+def denoise_gaussian(image: np.ndarray,
+                     ksize: int = 11) -> np.ndarray:
+    return cv2.GaussianBlur(image, (ksize, ksize), 0)
+
+
+def denoise_median(image: np.ndarray,
+                   ksize: int = 11) -> np.ndarray:
+    return cv2.medianBlur(image, ksize)
+
 # ---------- Resizing ----------
 #convert an image to a resized image
